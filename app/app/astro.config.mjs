a6b60f00ef58e83ba99764f73a59fa6635a4c7ce
Matiboux <matiboux@gmail.com>
2024-10-06 20:21:17 +0200

    Add app files

---
+++ app/app/astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		svelte(),
+		starlight({
+			title: 'Matiboux Guides',
+			components: {
+				SiteTitle: '~/components/overrides/SiteTitle.astro',
+			},
+			customCss: [
+				'./src/tailwind.css',
+			],
+		}),
+		tailwind({
+			applyBaseStyles: false, // Disable default base styles
+		}),
+	],
+})
