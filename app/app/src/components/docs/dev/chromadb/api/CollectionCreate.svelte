1f8ddf5fd88b744cfe456d40e6e8235fad8eddbd
Matiboux <matiboux@gmail.com>
2024-10-25 00:47:14 +0200

    Use input stores to fill api routes inputs

---
+++ app/app/src/components/docs/dev/chromadb/api/CollectionCreate.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { authTokenStore } from './auth-token.store'
+import { inputCollectionNameStore } from './input-collection-name.store'
+
+// Generate random id
+const id = Math.random().toString(36).substring(2)
+</script>
+
+<pre data-language="sh">
+curl -X POST -v \
+  https://chromadb.example.com/api/v1/collections \
+  -H "Authorization: Bearer <code>{$authTokenStore || '{AUTH_TOKEN}'}</code>" \
+  -H "Content-Type: application/json" \
+  -d '{'{'}
+    "name": "<code>{$inputCollectionNameStore || '{COLLECTION_NAME}'}</code>",
+    "metadata": {'{'}
+      "hnsw:space": "cosine"
+	{'}'},
+    "get_or_create": true
+  {'}'}'
+</pre>
+
+<style lang="scss">
+	pre {
+		width: 100%;
+		margin-top: 1rem;
+
+		code {
+			background-color: var(--sl-color-bg-inline-code);
+			margin-block: -0.125rem;
+			padding: 0.125rem 0.25rem;
+			font-size: var(--sl-text-code-sm);
+		}
+	}
+</style>
---
+++ app/app/src/components/docs/dev/chromadb/api/CollectionPeek.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { inputCollectionIdStore } from './input-collection-id.store'
+import { authTokenStore } from './auth-token.store'
+
+// Generate random id
+const id = Math.random().toString(36).substring(2)
+</script>
+
+<pre data-language="sh">
+curl -X POST -v \
+  https://chromadb.example.com/api/v1/collections/<code>{$inputCollectionIdStore || '{COLLECTION_ID}'}</code>/get \
+  -H "Authorization: Bearer <code>{$authTokenStore || '{AUTH_TOKEN}'}</code>" \
+  -H "Content-Type: application/json" \
+  -d '{'{'}
+    "limit": 1
+  {'}'}' \
+</pre>
+
+<style lang="scss">
+	pre {
+		width: 100%;
+		margin-top: 1rem;
+
+		code {
+			background-color: var(--sl-color-bg-inline-code);
+			margin-block: -0.125rem;
+			padding: 0.125rem 0.25rem;
+			font-size: var(--sl-text-code-sm);
+		}
+	}
+</style>
