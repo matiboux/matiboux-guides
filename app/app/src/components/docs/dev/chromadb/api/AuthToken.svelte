cc9d225eb573cf2a66aff593b4126ee5d5039011
Matiboux <matiboux@gmail.com>
2024-10-25 00:25:24 +0200

    Add inputs & store to set api routes inputs

---
+++ app/app/src/components/docs/dev/chromadb/api/AuthToken.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { authTokenStore } from './auth-token.store'
+
+	// Props
+	let label: string | undefined = undefined
+	export {
+		label,
+	}
+
+	// Generate random id
+	const id = Math.random().toString(36).substring(2)
+	</script>
+
+	<div class="input-group">
+		{#if label}
+			<label for={id}>{label}</label>
+		{/if}
+		<input
+			type="text"
+			id={id}
+			bind:value={$authTokenStore}
+			placeholder="secret_auth_token"
+		/>
+	</div>
+
+	<style lang="scss">
+		.input-group {
+			width: 100%;
+			margin-top: 1rem;
+
+			label {
+				display: block;
+				padding-bottom: 0.25rem;
+				font-size: 0.875rem;
+				color: var(--sl-color-gray-600);
+			}
+
+			input {
+				width: 100%;
+				padding: 0.25rem 0.5rem;
+				border: 1px solid var(--sl-color-hairline-light);
+				border-radius: 0.25rem;
+				outline-color: var(--sl-color-accent);
+			}
+		}
+	</style>
