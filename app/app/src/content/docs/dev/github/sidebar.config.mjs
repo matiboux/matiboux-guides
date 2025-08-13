05905fc09b943275d42328f717af9c25a789596d
Matiboux <matiboux@gmail.com>
2025-08-14 00:10:27 +0200

    Add github sidebar config

---
+++ app/app/src/content/docs/dev/github/sidebar.config.mjs
@@ -0,0 +1,30 @@
+import { sidebarLinks } from '~/content/docs/sidebar.config.mjs'
+
+/** @type {import('@astrojs/starlight/types').StarlightUserConfig} */
+const sidebar = [
+	...sidebarLinks,
+	{
+		label: '← Home',
+		slug: 'index',
+	},
+	{
+		label: '← Dev Guides',
+		slug: 'dev',
+	},
+	{
+		label: 'GitHub Guides',
+		collapsed: false,
+		items: [
+			{
+				label: 'GitHub Guides',
+				slug: 'dev/github',
+			},
+			{
+				label: 'GitHub repositories',
+				slug: 'dev/github/repo',
+			},
+		],
+	},
+]
+
+export default sidebar
