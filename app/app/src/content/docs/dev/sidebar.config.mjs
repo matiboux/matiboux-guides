c5c4e5f76e26f606e3adbf3dccea69a4931e2f91
Matiboux <matiboux@gmail.com>
2025-08-14 00:06:54 +0200

    Add dev sidebar

---
+++ app/app/src/content/docs/dev/sidebar.config.mjs
@@ -0,0 +1,38 @@
+import { sidebarLinks } from '~/content/docs/sidebar.config.mjs'
+
+/** @type {import('@astrojs/starlight/types').StarlightUserConfig} */
+const sidebar = [
+	...sidebarLinks,
+	{
+		label: '← Home',
+		slug: 'index',
+	},
+	{
+		label: 'Dev Guides',
+		collapsed: false,
+		items: [
+			{
+				label: 'Dev Guides',
+				slug: 'dev',
+			},
+			{
+				label: 'ChromaDB API →',
+				slug: 'dev/chromadb/api',
+			},
+			{
+				label: 'Docker Compose →',
+				slug: 'dev/docker/compose',
+			},
+			{
+				label: 'GitHub Guides →',
+				slug: 'dev/github',
+			},
+			{
+				label: 'Development Tools',
+				slug: 'dev/tools',
+			},
+		],
+	},
+]
+
+export default sidebar
